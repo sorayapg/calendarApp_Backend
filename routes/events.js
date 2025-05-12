2056374b766a3174e5200fcbc2e57cc0c7b00101
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-12 17:59:01 +0200

    feat: enhance event creation route with validation checks for title and dates

--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
-const router = Router();
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/isDate');
+const { validateFields } = require('../middlewares/validateFields');
 const { validateJWT } = require('../middlewares/validateJwt');
 const { getEvents, createEvent, updateEvent, deleteEvent} = require('../controllers/events');
 
+const router = Router();
 /* 
     Event Routes
     //*host: /api/events
@@ -17,7 +20,19 @@ router.get('/', getEvents );
 
 // Crear un nuevo evento 
 //*POST: /api/events
-router.post('/', createEvent );
+router.post(
+    '/', 
+    [
+        // validar el titulo
+        check( 'title', 'El titulo es obligatorio' ).not().isEmpty(),
+        // validar la fecha de inicio
+        check( 'start', 'La fecha de inicio es obligatoria' ).custom( isDate),
+        // validar la fecha de fin
+        check( 'end', 'La fecha de fin es obligatoria' ).custom( isDate ),
+        validateFields
+    ],
+    createEvent 
+);
 
 // Actualizar evento 
 //*PUT: /api/events/:id
