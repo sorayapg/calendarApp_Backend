c9360abf7275c48f5e1796425b2b63ac39f6a32a
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-13 18:20:22 +0200

    feat: refactor event controller methods to use async/await for improved error handling and add user authorization checks for update and delete operations

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,15 @@
 const { response } = require('express');
 const Event = require('../models/Event'); // Importar el modelo del evento
 
-const getEvents = ( req, res = response ) => {
+const getEvents = async ( req, res = response ) => {
 
-    return res.status(200).json({
+    //Obtener el listado de todos los eventos
+    const events = await Event.find()
+                              .populate('user', 'name') // para obtener el nombre del usuario que creo el evento
+                                
+    res.json({
         ok: true,
-        msj: 'getEvents',
+        events
     })
 }
 
@@ -37,21 +41,104 @@ const createEvent = async ( req, res = response ) => {
 }
 
 
-const updateEvent = ( req, res = response ) => {
+const updateEvent = async ( req, res = response ) => {
 
-    res.json({
-        ok: true,
-        msg: 'updateEvent',
-    })
+    const eventId = req.params.id; // obtenemos el id del evento
+    const uid = req.uid; // obtenemos el uid del usuario que quiere actualizar el evento
+
+    try {
+
+        // Verificar que el evento existe
+        const event = await Event.findById( eventId );
+        if ( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado por ese id'
+            });
+        }
+        /**
+         * Verificar que el usuario que quiere actualizar el evento es el mismo que lo creo,
+         * si no es el mismo, no podrá actualizar ya que no esta autorizado 
+         */
+
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios para editar este evento'
+            });
+        }
+
+        // Crear un nuevo objeto con los datos que se van actualizar
+        const newEvent = {
+            ...req.body,
+            user: uid // El usuario que actualiza el evento
+        }
+
+        // Actualizar el evento
+        const eventUpdated = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } );
+        res.json({
+            ok: true,
+            event: eventUpdated
+        });
+
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });        
+    }
+
+    
 }
 
 
-const deleteEvent = ( req, res = response ) => {
+const deleteEvent = async ( req, res = response ) => {
+    
+    const eventId = req.params.id // obtenemos el id del evento
+    const uid = req.uid; // Obtenemos el uid del usuario que quiere eliminar el evento
+    
 
-    res.json({
-        ok: true,
-        msg: 'deleteEvent',
-    })
+    try {
+
+        // verificar que el evento existe
+        const event = await Event.findById( eventId );
+        if ( !event ) {
+            return res. status(404).json({
+                ok: false,
+                msg: 'Evento no encontrado por ese id'
+            });        
+        }
+
+        /**
+         * verificar que el usuario que quiere eliminar el evento es el mismo que lo creo,
+         * si no es el mismo, no podrá eliminar ya que no esta autorizado
+         */
+
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegios para eliminaar este evento'
+            });
+        }
+
+        // Eliminar el evento 
+        await Event.findByIdAndDelete( eventId); 
+        
+        res.json({
+            ok: true
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        }); 
+    }
+    const event = Event.findById( eventId );
 }
 
 module.exports = {
